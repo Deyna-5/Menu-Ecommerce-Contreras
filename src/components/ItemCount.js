14993368951874f108a7f4750fe300857b7454ab
Deyna Contreras <alexahoshiop@gmail.com>
2022-04-24 22:14:37 -0400

    Entregable: Contador con boton

---
+++ src/components/ItemCount.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({stock, initial}) => {
+
+  const [count, setcount] = useState(initial);
+
+  const addCount = () => {
+    if(count >= stock){
+      setcount(stock)
+    }else{
+      setcount(count + 1);
+    }
+  };
+
+  const subtractCount = () => {
+    if(count <= 0){
+      setcount(0)
+    }else{
+      setcount(count - 1);
+    }
+  };
+
+  const restoreCount = () => {
+    setcount(initial)
+  };
+
+  return (
+    <>
+      <div className='text-center my-5 '>
+        <h2 className='text-2xl antialiased font-light tracking-wide'>Contador</h2>
+        <h3 className='text-4xl antialiased font-semibold'>{count}</h3>
+
+        <div className="btn-group my-5">
+          <button onClick={ addCount } className="btn bg-emerald-600 border-emerald-700">+</button>
+          <button onClick={ restoreCount } className="btn bg-emerald-600 border-emerald-700">Restaurar</button>
+          <button onClick={ subtractCount } className="btn bg-emerald-600 border-emerald-700">-</button>
+        </div>
+      </div>
+
+      
+    </>
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 const ItemListContainer = (props) => {
+
   return (
     <>
       <div className="text-center mt-10">
         <h1 className="text-5xl font-mono font-semibold text-emerald-600">{props.greeting}</h1>
       </div>
+
+      <ItemCount stock={15} initial={0} />
     </>
   )
 }
